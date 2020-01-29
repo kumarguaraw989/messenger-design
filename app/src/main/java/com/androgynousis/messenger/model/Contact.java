865cfd07931ab820586e5aff1ce6313200d6d275
Kaushik Burkule <kaushikburkule@gmail.com>
2020-01-29 15:29:51 +0530

    Data/Holders
    Signed-off-by: Kaushik Burkule <kaushikburkule@gmail.com>

---
+++ app/src/main/java/com/androgynousis/messenger/model/Contact.java
@@ -0,0 +1,23 @@
+package com.androgynousis.messenger.model;
+
+import android.support.annotation.DrawableRes;
+
+public class Contact {
+
+    String name;
+    int image;
+
+    public int getImage(){
+        return image;
+    }
+    public void setImage(@DrawableRes int img){
+        image = img;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
