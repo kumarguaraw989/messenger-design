66e4fe7d24aa4b061f85bbe9dc19405a6a656c4e
Kaushik Burkule <kaushikburkule@gmail.com>
2020-01-29 15:31:34 +0530

    Activities/Classes
    Signed-off-by: Kaushik Burkule <kaushikburkule@gmail.com>

---
+++ app/src/main/java/com/androgynousis/messenger/view/activities/SetupPhoneNumber.java
@@ -0,0 +1,26 @@
+package com.androgynousis.messenger.view.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.androgynousis.messenger.R;
+
+public class SetupPhoneNumber extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_phone_number);
+
+        setupToolbar(R.id.toolbar, "Setting Messenger");
+        Button button = (Button) findViewById(R.id.bt_continue);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SetupPhoneNumber.this, MainActivity.class));
+            }
+        });
+    }
+}
