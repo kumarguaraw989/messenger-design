cc2f8088b544895c0035e82f72d7113b7df51732
Kaushik Burkule <kaushikburkule@gmail.com>
2020-01-29 15:33:33 +0530

    Holder/Classes
    Signed-off-by: Kaushik Burkule <kaushikburkule@gmail.com>

---
+++ app/src/main/java/com/androgynousis/messenger/view/holder/HolderDate.java
@@ -0,0 +1,25 @@
+package com.androgynousis.messenger.view.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.androgynousis.messenger.R;
+
+public class HolderDate extends RecyclerView.ViewHolder {
+
+    private TextView date;
+
+    public HolderDate(View v) {
+        super(v);
+        date = (TextView) v.findViewById(R.id.tv_date);
+    }
+
+    public TextView getDate() {
+        return date;
+    }
+
+    public void setDate(TextView date) {
+        this.date = date;
+    }
+}
